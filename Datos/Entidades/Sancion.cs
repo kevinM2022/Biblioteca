49132e10b0244835f15e89ea4c5190c018f14060
kevinM2022 <montenegrokevin026@gmail.com>
2023-11-19 21:00:14 -0600

    añadido Sancion

---
+++ Datos/Entidades/Sancion.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Entidades
+{
+    //Una entidad por cada tabla de sql, contendra los atributos de las tablas
+    public class Sancion
+    {
+        public int IdSancion { get; set; }
+        public string Concepto { get; set; }
+        public string Monto { get; set; }
+
+    }
+}
---
+++ Datos/Repositorios/SancionRepository.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Repositories;
+using Datos.Interfaz;
+using Datos.Entidades;
+
+namespace Datos.Repositorios
+{
+    public class SancionRepository: MasterRepository, ISancionRepository
+    {
+        private string selectAll;
+        private string insert;
+        private string update;
+        private string delete;
+
+        public SancionRepository()
+        {
+            selectAll = "SELECT * FROM Multas";
+            insert = "INSERT INTO Multas VALUES ()";
+            update = "UPDATE Products SET";
+            delete = "DELETE FROM Multas WHERE IdMulta = @IdMulta";
+        }
+
+        public int Add(Sancion entity)
+        {
+            parameters = new List<SqlParameter>();   
+            parameters.Add(new SqlParameter("@IdSancion", entity.IdSancion));         
+            parameters.Add(new SqlParameter("@Concepto", entity.Concepto));
+            parameters.Add(new SqlParameter("@Monto", entity.Monto));
+
+            return ExecuteNonQuery(insert);
+        }
+
+        public int Adit(Sancion entity)
+        {
+            parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("@IdLibro", entity.IdSancion));
+            parameters.Add(new SqlParameter("@Concepto", entity.Concepto));
+            parameters.Add(new SqlParameter("@Monto", entity.Monto));
+
+            return ExecuteNonQuery(update);
+        }
+
+        public IEnumerable<Sancion> GetAll()
+        {
+            var tableResult = ExecuteReader(selectAll);
+            var listsancion = new List<Sancion>();
+            foreach (DataRow item in tableResult.Rows)
+            {
+                listsancion.Add(new Sancion
+                {
+                    IdSancion = Convert.ToInt32(item[0]),
+                    Concepto = item[1].ToString(),
+                    Monto = item[2].ToString(),
+                });
+            }
+            return listsancion;
+        }
+
+        public int Remove(int id)
+        {
+            parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("@IdSancion", id));
+
+            return ExecuteNonQuery(delete);
+        }
+    }
+}
