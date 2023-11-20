65eda6884e5d69783bc1c017faf80fb33a1dcea8
kevinM2022 <montenegrokevin026@gmail.com>
2023-11-19 22:44:51 -0600

    mas

---
+++ Biblioteca/Controllers/LibroController.cs
@@ -0,0 +1,33 @@
+﻿using Biblioteca.Entidades;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.Controllers
+{
+    [Route("api/Libro")]
+    public class LibroController : Controller
+    {
+        private readonly ILogger<LibroController> logger;
+        private readonly LibroEP libro;
+        private readonly LibroDTO libroDTO;
+
+        public LibroController(ILogger<LibroController> logger, LibroEP libro,LibroDTO libroDTO)
+        {
+            this.logger = logger;
+            this.libro = libro;
+            this.libroDTO = libroDTO;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<LibroDTO>>> Get()
+        {
+            return await libro.Get();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> Post([FromBody] LibroCDTO libroCDTO)
+        {
+            return await libro.Post(libroCDTO);
+        }
+    }
+}
---
+++ Biblioteca/Controllers/PrestamoController.cs
@@ -0,0 +1,33 @@
+﻿using Biblioteca.Entidades;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.Controllers
+{
+    [Route("api/Prestamo")]
+    public class PrestamoController : Controller
+    {
+        private readonly ILogger<PrestamoController> logger;
+        private readonly PrestamoEP prestamo;
+        private readonly PrestamoDTO prestamoDTO;
+
+        public PrestamoController(ILogger<PrestamoController> logger, PrestamoEP prestamo,PrestamoDTO prestamoDTO)
+        {
+            this.logger = logger;
+            this.prestamo = prestamo;
+            this.prestamoDTO = prestamoDTO;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PrestamoDTO>>> Get()
+        {
+            return await prestamo.Get();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> Post([FromBody] PrestamoCDTO prestamoCDTO)
+        {
+            return await prestamo.Post(prestamoCDTO);
+        }
+    }
+}
