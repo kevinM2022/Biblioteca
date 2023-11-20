af893d0f50d76c8038d6b33913601826980723b2
kevinM2022 <montenegrokevin026@gmail.com>
2023-11-19 23:07:49 -0600

    noshe

---
+++ Biblioteca/Controllers/SancionController.cs
@@ -0,0 +1,33 @@
+﻿using Biblioteca.Entidades;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.Controllers
+{
+    [Route("api/Sancion")]
+    public class SancionController : Controller
+    {
+        private readonly ILogger<SancionController> logger;
+        private readonly SancionEP sancion;
+        private readonly SancionDTO sancionDTO;
+
+        public SancionController(ILogger<SancionController> logger, SancionEP sancion,SancionDTO sancionDTO)
+        {
+            this.logger = logger;
+            this.sancion = sancion;
+            this.sancionDTO = sancionDTO;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SancionDTO>>> Get()
+        {
+            return await sancion.Get();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> Post([FromBody] SancionCDTO sancionCDTO)
+        {
+            return await sancion.Post(sancionCDTO);
+        }
+    }
+}
