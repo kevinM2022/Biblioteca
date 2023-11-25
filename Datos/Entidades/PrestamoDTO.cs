4ec63f17b64ad4c380aad46e723efb87592e0bd0
kevinM2022 <montenegrokevin026@gmail.com>
2023-11-25 06:35:16 -0600

    Convertido a String con validacion JSON

--- Datos/Entidades/PrestamoDTO.cs
+++ Datos/Entidades/PrestamoDTO.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Datos.Entidades
@@ -12,7 +15,24 @@ namespace Datos.Entidades
         public int IdPrestamo { get; set; }
         public string CantidadLibros { get; set; }
         public string EstadoPrestamo { get; set; }
-        public DateOnly FechaPrestamo { get; set; }
-        public DateOnly Devolucion { get; set; }
+        public DateTime FechaPrestamo { get; set; }
+        public DateTime Devolucion { get; set; }
+    }
+
+    public class JsonStringDateConverter : JsonConverter<DateTime>
+{
+    private const string DateFormat = "dd-MM-yyyy";
+
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateTime.ParseExact(reader.GetString(), DateFormat, CultureInfo.InvariantCulture);
+    }
+
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(DateFormat));
+    }
+
     }
 }
